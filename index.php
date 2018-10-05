af058bd2357c8847a6431c706a52f4f460613cd9
wstechsupport <wstechsupport@webspiders.com>
2018-10-05 19:05:11 +0530

    Commting again after push to GIThub

--- index.php
+++ index.php
@@ -5,3 +5,5 @@ echo "Manoranjan Barick. Tutorials of GIT";
 echo "GIT Commit changes using --amend command";
 
 echo "GIT Commit changes using After push";
+
+echo "GIT Commit changes using After push - pushing second commit";
